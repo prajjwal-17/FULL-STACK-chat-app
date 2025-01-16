6d10331667f4a0b9fe4f3033048396e24922ec94
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-17 02:27:15 +0530

    Created the updateProfile endpoiny

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
 import { generateToken } from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
 
@@ -121,5 +122,20 @@ export const logout = async(req, res) => {
 };
 
 export const updateProfile=async(req,res)=>{
+    try {
+       const {profilePic}=req.body;
+       const userId=req.user._id;
 
+       if(!profilePic){
+        return res.status(400).json({message: "Profile pic is required"});
+       }
+
+       const uploadResponse=await cloudinary.uploader.upload(profilePic);
+       const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+       res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("error in update profile:",error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
+ 
