4a590fe67b66eab670fc4a7613dc8868c62649ff
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-17 01:38:19 +0530

    Signup, login and logout root created and tested

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,10 +83,39 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("Login Route");
+export const login = async(req, res) => {
+    const {email,password}=req.body;
+
+    try {
+        const user= await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials" })
+        }
+        const isPasswordCorrect=await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){  
+            return res.status(400).json({message: "Invalid Credentials" })
+        }
+
+        generateToken(user._id,res)
+        res.status(200).json({
+            _id:user._id,
+            email: user.email,
+            profilePic: user.profilePic || null,
+            fullName: user.fullName
+        })
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({message: "Internal Server Error"}) 
+    }
+
 };
 
-export const logout = (req, res) => {
-    res.send("Logout Route");
+export const logout = async(req, res) => {
+    try {
+        res.cookie("jwt","",{maxAge:0})
+        res.status(200).json({message:"Logged out Successfully"})
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message: "Internal Server Error"}) 
+    }
 };
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 export const generateToken=(userId,res)=>{
+   console.log("Respose is :"+res)
    const token=jwt.sign({userId},process.env.JWT_SECRET,{
     expiresIn:"7d"
    })
 
    res.cookie("jwt",token,{
       maxAge: 7*24*60*60*1000,
-      httpsOnly: true, //prevents xss attacks
+      httpOnly: true, //prevents xss attacks
       sameSite: "strict", // CSRF attacks
       secure: process.env.NODE_ENV!=="development"
    })
