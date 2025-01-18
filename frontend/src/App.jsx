1659a4a5074cf3db71fb42a99fc933392afa6e36
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-19 03:31:51 +0530

    Created the boiler plate for navbar component and pages under it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { Navbar } from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import  {HomePage} from './pages/HomePage'
+import { SignUpPage} from './pages/SignUpPage'
+import { LoginPage } from './pages/LoginPage'
+import { ProfilePage } from './pages/ProfilePage'
+import SettingsPage from './pages/SettingPage'
+
 const App=()=>{
   return (
-    <div className="text-red-500">Hello
-    <button className="btn">Button</button>
-    <button className="btn btn-neutral">Neutral</button>
-    <button className="btn btn-primary">Primary</button>
-    <button className="btn btn-secondary">Secondary</button>
-    <button className="btn btn-accent">Accent</button>
-    <button className="btn btn-ghost">Ghost</button>
-    <button className="btn btn-link">Link</button>
+    <div >
+      <Navbar/>
+      <Routes>
+         <Route path="/"element={<HomePage/>}/>
+         <Route path="/signup"element={<SignUpPage/>}/>
+         <Route path="/login"element={<LoginPage/>}/>
+         <Route path='/settings'element={ <SettingsPage/> }/>
+         <Route path="/profile"element={<ProfilePage/>}/>
+      </Routes>
     </div>
 
   )
