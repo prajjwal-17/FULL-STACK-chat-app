6f7f6f0b342c73fea24bfc7f1b0f5916792af546
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-19 05:48:01 +0530

    Creating strucutre of the frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 dotenv.config();
 const app=express();
@@ -13,6 +14,10 @@ const PORT=process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials: true,
+}));
 
 app.get("/test", (req, res) => {
     res.json({ message: "Test route is working!" });
