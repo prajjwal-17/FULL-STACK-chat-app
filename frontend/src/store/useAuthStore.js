454c11274d9032af906be66b790d420d2c8f86ca
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-19 06:28:18 +0530

    Started building signup page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -21,5 +21,8 @@ export const useAuthStore=create((set)=>({
         }finally{
             set({isCheckingAuth: false})
         }
+    },
+    signup : async(data)=>{
+        //logic here
     }
 }))
