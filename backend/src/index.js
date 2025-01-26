3ee19be3c0c63bce24762d68897f34af033f211b
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-01-27 02:02:46 +0530

    Implementing Socket in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import cors from 'cors'
+import { app,server } from "./lib/socket.js";
 
 dotenv.config();
-const app=express();
+
 
 const PORT=process.env.PORT
 
@@ -26,7 +27,7 @@ app.get("/test", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("Server is running on port PORT: "+PORT);
     connectDB();
 });
