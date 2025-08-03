3bc5176cbffa61dda190cef8e68c180dd7306301
Prajjwal Rawat <139827569+prajjwal-17@users.noreply.github.com>
2025-08-04 02:28:21 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-export const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE === "development"? "http://localhost:5001/api":"/api",
-    withCredentials: true , //sending cookies with every request
-})
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://full-stack-chat-app-gyuv.onrender.com/api"; // <-- backend hosted on Render
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, // sending cookies with every request
+});
